a71cb4d39f426f33c75b6f2f05e8f926e7ec27a9
ChrisMD2 <Charlie.Mike.1991@gmail.com>
2021-07-29 16:34:18 -0400

    Each event should only occur once and therfore should have a location

--- lib/networking_log/nodes/event.ex
+++ lib/networking_log/nodes/event.ex
@@ -9,8 +9,10 @@ defmodule NetworkingLog.Nodes.Event do
     field :date,              :string
     field :time,              :string
 
+    belongs_to :location,     Nodes.Place
+
     many_to_many :interests,  Nodes.Interest, join_through: "interests"
-    many_to_many :places,     Nodes.Place, join_through: "places"
+    # many_to_many :places,     Nodes.Place, join_through: "places"
     many_to_many :groups,     Nodes.Group, join_through: "groups"
     many_to_many :people,     Nodes.Person, join_through: "people"
     many_to_many :note,       Nodes.Note, join_through: "note"
@@ -20,7 +22,7 @@ defmodule NetworkingLog.Nodes.Event do
 
   def changeset(event, attrs) do
     event
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
+    |> cast(attrs, [:name, :date, :time, ])
+    |> validate_required([:name, :location])
   end
 end
