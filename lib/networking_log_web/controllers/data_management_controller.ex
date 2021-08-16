9433be3d0532eca59274ce475169b89de3293ff4
ChrisMD2 <Charlie.Mike.1991@gmail.com>
2021-08-16 15:58:59 -0400

    Form configured, issues b/c of assets fixed

--- lib/networking_log_web/controllers/data_management_controller.ex
+++ lib/networking_log_web/controllers/data_management_controller.ex
@@ -5,16 +5,11 @@ defmodule NetworkingLogWeb.DataManagementController do
     render(conn, "data_management_live.html")
   end
 
-  @impl true
-  def mount(_params, _session, socket) do
-    IO.write("\n\nFLAG\n\n")
-    mounted_socket = socket
-    |> assign(:people, Nodes.get_all_people)
-    |> assign(:notes, Nodes.get_all_notes)
-    |> assign(:person_to_notes, Nodes.get_all_person_to_notes)
-
-    IO.inspect(mounted_socket, label: "new socket in mount")
-
-    {:ok, mounted_socket}
+  def recieve(conn, params) do
+    IO.inspect(conn, label: "conn in DataManagementController")
+    IO.inspect(params, label: "params in DataManagementController")
+    conn
+    |> put_status(201)
+    |> json(%{})
   end
 end
--- lib/networking_log_web/templates/data_management/data_management_live.ex
+++ lib/networking_log_web/templates/data_management/data_management_live.ex
@@ -3,15 +3,40 @@ defmodule NetworkingLogWeb.DataManagementLive do
   use Phoenix.LiveView
   alias NetworkingLog.Nodes
 
+  def dev_test(conn, params) do
+    IO.inspect(conn, label: "conn in DataManagementController")
+    IO.inspect(params, label: "params in DataManagementController")
+  end
+
   @impl true
   def mount(_params, _session, socket) do
     mounted_socket = socket
     |> assign(:people, Nodes.get_all_people)
-    |> assign(:people, Nodes.get_all_notes)
-    |> assign(:people, Nodes.get_all_person_to_notes)
+    |> assign(:note, Nodes.get_all_notes)
+    |> assign(:person_to_notes, Nodes.get_all_person_to_notes)
+    |> assign(changeset: NetworkingLog.Nodes.Person.changeset(%NetworkingLog.Nodes.Person{}, %{}) )
 
     IO.inspect(mounted_socket, label: "new socket in mount")
 
     {:ok, mounted_socket}
+  end
+
+  @impl true
+  def handle_event("delete", params, socket) do
+    IO.inspect(params, label: "params")
+    IO.inspect(socket, label: "socket")
+    {:noreply, socket}
+  end
+  @impl true
+  def handle_event("add_new_person", params, socket) do
+    IO.inspect(params, label: "params")
+    IO.inspect(socket, label: "socket")
+    {:noreply, socket}
+  end
+  @impl true
+  def handle_event("", params, socket) do
+    IO.inspect(params, label: "params")
+    IO.inspect(socket, label: "socket")
+    {:noreply, socket}
   end
 end
