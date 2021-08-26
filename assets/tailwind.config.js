84e84e0a26c17a3811c7d7ae406d167d4ff3877a
ChrisMD2 <Charlie.Mike.1991@gmail.com>
2021-08-25 21:55:40 -0400

    Set tailwind up to be more flexible in addition to testing along the way Try adding a new class to christenson.css with colors.NEWCOLOR.* similarly to :root, but .th-NEWCOLOR instead, to call different themes in tailwind directly

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,10 +7,33 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'tan': '#E3E6C0',
+      'olive': '#BEC645',
+      'sea-green': '#819D49',
+      'forest-green': '#5A8359',
+      'night-green': '#112810',
+      green: {
+        pale: '#E3E6C0',
+        light: '#BEC645',
+        base: '#819D49',
+        dark: '#5A8359',
+        darkest: '#112810',
+      },
+      'th-pale': 'var(--pale)',
+      'th-light': 'var(--light)',
+      'th-base': 'var(--base)',
+      'th-dark': 'var(--dark)',
+      'th-darkest': 'var(--darkest)',
+
+    },
+    extend: {
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      textDecoration: ['active'],
+    },
   },
   plugins: [],
 }
