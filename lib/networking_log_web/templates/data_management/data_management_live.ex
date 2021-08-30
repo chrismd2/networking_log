2d175b7def5d4f8a12facca6317e24edd9e6d0de
ChrisMD2 <Charlie.Mike.1991@gmail.com>
2021-08-30 14:26:54 -0400

    Connect now links people to notes

--- lib/networking_log_web/templates/data_management/data_management_live.ex
+++ lib/networking_log_web/templates/data_management/data_management_live.ex
@@ -24,10 +24,10 @@ defmodule NetworkingLogWeb.DataManagementLive do
     "selectable-btn"
   end
   def selectable_format(list, element) do
-    IO.inspect(element, label: "element in selectable_format")
-    IO.inspect(list, label: "list in selectable_format")
+    # IO.inspect(element, label: "element in selectable_format")
+    # IO.inspect(list, label: "list in selectable_format")
     [h|t] = list
-    IO.inspect(h.id == element.id, label: "bool val in selectable_format")
+    # IO.inspect(h.id == element.id, label: "bool val in selectable_format")
     if h.id == element.id do
       "selected-btn"
     else
@@ -64,22 +64,22 @@ defmodule NetworkingLogWeb.DataManagementLive do
   end
   @impl true
   def handle_event("select_person", params, socket) do
-    IO.inspect(params, label: "params in handle_event(select_person)")
+    # IO.inspect(params, label: "params in handle_event(select_person)")
 
     {:ok, value} = Map.fetch(params, "value")
     if is_binary(value) do
       {value, _string_tail} = Integer.parse(value)
-      IO.inspect(value, label: "value in select_person")
+      # IO.inspect(value, label: "value in select_person")
       currently_selected_person_list = socket.assigns.selected_person
       new_person = Nodes.read_person(value)
 
       socket = socket
       |> assign(:selected_person, select_person_helper(currently_selected_person_list, new_person))
 
-      IO.inspect(socket, label: "new socket in handle_event(select_person)")
+      # IO.inspect(socket, label: "new socket in handle_event(select_person)")
       {:noreply, socket}
     else
-      IO.write("ERROR: value is not an int\n")
+      IO.write("ERROR: value is not a binary\n")
       IO.inspect(value, label: "actual value")
       {:noreply, socket}
     end
@@ -88,7 +88,7 @@ defmodule NetworkingLogWeb.DataManagementLive do
   def handle_event("delete", _params, socket) do
     # IO.inspect(params, label: "params in delete")
     # IO.inspect(socket, label: "socket in delete")
-    IO.inspect(socket.assigns.selected_person, label: "selected_person buttons for deletion")
+    # IO.inspect(socket.assigns.selected_person, label: "selected_person buttons for deletion")
     Nodes.delete_person(socket.assigns.selected_person)
     Nodes.delete_note(socket.assigns.selected_note)
     socket = socket
@@ -97,7 +97,7 @@ defmodule NetworkingLogWeb.DataManagementLive do
     |> assign(:person_to_notes, Nodes.get_all_person_to_notes)
     |> assign(:selected_person, [])
     |> assign(:selected_note, [])
-    IO.inspect(socket, label: "new socket in delete")
+    # IO.inspect(socket, label: "new socket in delete")
     {:noreply, socket}
   end
   @impl true
@@ -112,8 +112,8 @@ defmodule NetworkingLogWeb.DataManagementLive do
   end
   @impl true
   def handle_event("add_new_note", params, socket) do
-    IO.inspect(params, label: "params in add_new_note")
-    IO.inspect(socket, label: "socket in add_new_note")
+    # IO.inspect(params, label: "params in add_new_note")
+    # IO.inspect(socket, label: "socket in add_new_note")
     NetworkingLog.Nodes.create_note(params)
 
     socket = socket
@@ -121,33 +121,81 @@ defmodule NetworkingLogWeb.DataManagementLive do
     |> assign(:notes, Nodes.get_all_notes)
     |> assign(:person_to_notes, Nodes.get_all_person_to_notes)
 
-    IO.inspect(socket, label: "new socket in add_new_note")
+    # IO.inspect(socket, label: "new socket in add_new_note")
     {:noreply, socket}
   end
   @impl true
   def handle_event("select_note", params, socket) do
-    IO.inspect(params, label: "params in handle_event(select_note)")
+    # IO.inspect(params, label: "params in handle_event(select_note)")
 
     {:ok, value} = Map.fetch(params, "value")
     {value, _string_tail} = Integer.parse(value)
-    IO.inspect(value, label: "value in select_note")
+    # IO.inspect(value, label: "value in select_note")
     currently_selected_note_list = socket.assigns.selected_note
     new_note = Nodes.read_note(value)
 
     socket = socket
     |> assign(:selected_note, select_person_helper(currently_selected_note_list, new_note))
 
-    IO.inspect(socket, label: "new socket in handle_event(select_note)")
+    # IO.inspect(socket, label: "new socket in handle_event(select_note)")
     {:noreply, socket}
+  end
+  defp update_selected([]) do
+    []
+  end
+  defp update_selected(currently_selected = [record | _]) do
+    tail = List.delete(currently_selected, record)
+    if tail != [] do
+      if Map.has_key?(record, :text) do
+        [Nodes.read_note(record.id)] ++ update_selected(tail)
+      else
+        [Nodes.read_person(record.id)] ++ update_selected(tail)
+      end
+    else
+      if Map.has_key?(record, :text) do
+        [Nodes.read_note(record.id)]
+      else
+        [Nodes.read_person(record.id)]
+      end
+    end
+  end
+  defp connect_helper(person_list = [p_h | _] , note_list = [n_h | _]) do
+    #  BUG: This is a very good spot to spawn processes
+    #       to do updates based on different lists concurrently
+    person_list = List.delete(person_list, p_h)
+    if person_list != [] do
+      # IO.inspect(person_list, label: "person list not empty")
+      connect_helper(person_list, note_list)
+    end
+    note_list = List.delete(note_list, n_h)
+    if note_list != [] do
+      # IO.inspect(note_list, label: "note list not empty")
+      connect_helper([p_h], note_list)
+    end
+
+    Nodes.update_person_notes(p_h, n_h)
+    # |> IO.inspect(label: "tried updating ptn in connect_helper")
+    # IO.inspect(p_h, label: "p_h in connect_helper")
+    # IO.inspect(n_h, label: "n_h in connect_helper")
   end
   @impl true
   def handle_event("connect", params, socket) do
-    IO.inspect(params, label: "params in connect")
-    IO.inspect(socket, label: "socket in connect")
+    assigns = socket.assigns
+    # IO.inspect(assigns, label: "assigns in connect")
+    # IO.inspect(assigns.selected_person, label: "assigns.selected_person in connect")
+    # IO.inspect(assigns.selected_note, label: "assigns.selected_note in connect")
+    connect_helper(assigns.selected_person, assigns.selected_note)
+    update_selected(assigns.selected_person)
+    # |> IO.inspect(label: "update_selected return value")
+
+    # IO.inspect(assigns.selected_person, label: "originally selected person")
+
     socket = socket
     |> assign(:people, Nodes.get_all_people)
     |> assign(:notes, Nodes.get_all_notes)
     |> assign(:person_to_notes, Nodes.get_all_person_to_notes)
+    |> assign(:selected_person, update_selected(assigns.selected_person))
+    # IO.inspect(socket, label: "modified socket")
     {:noreply, socket}
   end
   @impl true
